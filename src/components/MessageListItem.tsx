663ab3e533fb74d162ee6814ab1786edb0c5e348
Saurabh <saurabkumar679@gmail..com>
2025-06-18 16:47:26 +0530

    markdown

--- src/components/MessageListItem.tsx
+++ src/components/MessageListItem.tsx
@@ -1,4 +1,65 @@
 import { View, Text } from "react-native";
+import Markdown from "react-native-markdown-display";
+
+const markdownStyles = {
+  body: {
+    color: "white",
+  },
+  code_inline: {
+    backgroundColor: "#1e1e1e",
+    color: "white",
+    borderRadius: 8,
+    padding: 10,
+    marginVertical: 10,
+    lineHeight: 20,
+  },
+  code_block: {
+    backgroundColor: "#1e1e1e",
+    color: "white",
+    borderRadius: 8,
+    padding: 10,
+    marginVertical: 10,
+    lineHeight: 20,
+  },
+  fence: {
+    backgroundColor: "#1e1e1e",
+    color: "white",
+    borderRadius: 8,
+    padding: 10,
+    marginVertical: 10,
+    lineHeight: 20,
+  },
+  blockquote: {
+    backgroundColor: "#2d2d2d",
+    borderLeftColor: "#4d4d4d",
+    borderLeftWidth: 4,
+    paddingLeft: 16,
+    paddingVertical: 8,
+    marginVertical: 8,
+  },
+  bullet_list: {
+    marginVertical: 8,
+  },
+  ordered_list: {
+    marginVertical: 8,
+  },
+  list_item: {
+    marginVertical: 4,
+  },
+  hr: {
+    backgroundColor: "#4d4d4d",
+    marginVertical: 16,
+  },
+  heading1: {
+    marginVertical: 10,
+  },
+  heading2: {
+    marginVertical: 10,
+  },
+  heading3: {
+    marginVertical: 10,
+  },
+};
 
 export default function MessageListItem({ item }: any) {
   const { message, role } = item;
@@ -16,7 +77,7 @@ export default function MessageListItem({ item }: any) {
         style={[
           {
             paddingHorizontal: 10,
-            paddingVertical: 5,
+            paddingVertical: 2,
             marginBottom: 5,
             maxWidth: "70%",
             borderTopStartRadius: 10,
@@ -25,7 +86,7 @@ export default function MessageListItem({ item }: any) {
           iUser && { backgroundColor: "gray" },
         ]}
       >
-        <Text style={[{ color: "white", fontSize: 16 }]}>{message}</Text>
+        <Markdown style={markdownStyles}>{message}</Markdown>
       </View>
     </View>
   );
